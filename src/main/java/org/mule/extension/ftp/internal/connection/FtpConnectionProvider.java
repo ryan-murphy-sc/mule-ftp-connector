785864eedc5ac3de71e59b0579d47f99351148f3
Ryan Murphy <rmurp@softchoice.com>
2020-06-25 09:58:00 -0400

    setDefaultTimeout to prevent hanging on dispose

--- src/main/java/org/mule/extension/ftp/internal/connection/FtpConnectionProvider.java
+++ src/main/java/org/mule/extension/ftp/internal/connection/FtpConnectionProvider.java
@@ -179,6 +179,7 @@ public class FtpConnectionProvider extends FileSystemProvider<FtpFileSystem> imp
     client.setControlEncoding(controlEncoding);
     if (getConnectionTimeout() != null && getConnectionTimeoutUnit() != null) {
       client.setConnectTimeout(new Long(getConnectionTimeoutUnit().toMillis(getConnectionTimeout())).intValue());
+      client.setDefaultTimeout(new Long(getConnectionTimeoutUnit().toMillis(getConnectionTimeout())).intValue());
     }
 
     if (LOGGER.isDebugEnabled()) {
